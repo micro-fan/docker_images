4bfdffd8638e9642e8ae4744c6449e8479d9d6fa
Kirill Pinchuk <cybergrind@gmail.com>
2019-04-23 15:45:23 +0300

    Update example app logging import

--- app/example.py
+++ app/example.py
@@ -1,11 +1,12 @@
-import time
 import logging
-from tipsi_tools.logging import setup_logger
+import time
 
-setup_logger('some_name')
-log = logging.getLogger('example')
+from tipsi_tools.tipsi_logging import setup_logger
+
+setup_logger("some_name")
+log = logging.getLogger("example")
 
 while 1:
-    log.debug('This is example log debug')
-    print('This is example print')
+    log.debug("This is example log debug")
+    print("This is example print")
     time.sleep(1)
