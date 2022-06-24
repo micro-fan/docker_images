a89a5444814396d98d41255017283d204256f308
Kirill Pinchuk <cybergrind@gmail.com>
2022-06-24 15:45:20 +0300

    ubuntu: focal => jammy

--- base_python/install_basic_packages.sh
+++ base_python/install_basic_packages.sh
@@ -10,10 +10,10 @@ apt-get install -y  --no-install-recommends \
         software-properties-common
 
 curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-add-apt-repository 'deb http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main'
+add-apt-repository 'deb http://apt.postgresql.org/pub/repos/apt/ jammy-pgdg main'
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-add-apt-repository ppa:deadsnakes/ppa
+# add-apt-repository ppa:deadsnakes/ppa
 
 apt-get update && apt-get install -y --no-install-recommends docker-ce
 
@@ -35,14 +35,14 @@ apt-get install -y --no-install-recommends \
     postgresql-client-13 \
     vim
 
-apt -y install python${PY_VERSION} python${PY_VERSION}-venv python${PY_VERSION}-dev
+apt -y install python${PY_VERSION} python${PY_VERSION}-venv python${PY_VERSION}-dev python3-pip
 update-alternatives --install /usr/bin/python python /usr/bin/python${PY_VERSION} 1
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python${PY_VERSION} 1
 
-python3 -m ensurepip --upgrade
+#python3 -m ensurepip --upgrade
 
 python3 -m pip install -U setuptools wheel supervisor
-python3 -m pip install pillow uwsgi psycopg2-binary fan-tools==3.* awscli==1.20.13 docker-compose
+python3 -m pip install pillow uwsgi psycopg2-binary fan-tools==3.* awscli docker-compose
 
 cd /usr/local/bin/
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
