bf30804a6a5fa591c8ee252a1f45c262dc1e0480
Kirill Pinchuk <cybergrind@gmail.com>
2022-07-05 21:12:19 +0300

    helm: force v3.8.2

--- base_python/install_helm_packages.sh
+++ base_python/install_helm_packages.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$BUILD_HELM" = "1" ]; then
-    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash -s -- --version v3.8.2
     curl -L https://github.com/helmfile/helmfile/releases/download/v0.145.1/helmfile_0.145.1_linux_amd64.tar.gz -o helmfile.tgz
     tar xzf helmfile.tgz
     mv helmfile /usr/bin
