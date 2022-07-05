0ee2085e1f47b240d1760a466e6c6f8cae84ce2b
Kirill Pinchuk <cybergrind@gmail.com>
2022-07-05 18:58:39 +0300

    helm: add helmfile

---
+++ base_python/install_helm_packages.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$BUILD_HELM" = "1" ]; then
+    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+    curl -L https://github.com/helmfile/helmfile/releases/download/v0.145.1/helmfile_0.145.1_linux_amd64.tar.gz -o helmfile.tgz
+    tar xzf helmfile.tgz
+    mv helmfile /usr/bin
+    rm helmfile.tgz
+fi
