a54ec84a71c2e6719a6e83893a4e1b5fec16c435
Kirill Pinchuk <cybergrind@gmail.com>
2023-06-30 11:48:51 +0300

    Update helm and helmfile

--- base_python/install_helm_packages.sh
+++ base_python/install_helm_packages.sh
@@ -2,8 +2,10 @@
 set -e
 
 if [ "$BUILD_HELM" = "1" ]; then
-    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash -s -- --version v3.8.2
-    curl -L https://github.com/helmfile/helmfile/releases/download/v0.145.1/helmfile_0.145.1_linux_amd64.tar.gz -o helmfile.tgz
+    HELM_VERSION=3.12.1
+    HELMFILE_VERSION=0.155.0
+    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash -s -- --version v${HELM_VERSION}
+    curl -L https://github.com/helmfile/helmfile/releases/download/v${HELMFILE_VERSION}/helmfile_${HELMFILE_VERSION}_linux_amd64.tar.gz -o helmfile.tgz
     tar xzf helmfile.tgz
     mv helmfile /usr/bin
     rm helmfile.tgz
